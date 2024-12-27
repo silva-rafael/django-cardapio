5f6378df7e04d4a4b0e3e79f429f18cf4cb20688
silva-rafael <rafaelribeirodasilva7@gmail.com>
2024-12-27 10:49:54 -0300

    Comecei a criar os modelos

--- app/models.py
+++ app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+#Área do admin
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=5, decimal_places=2)
+    img = models.ImageField(upload_to='image/%Y/%m', blank=True, null=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+    
+class Complemento(models.Model):
+    nome = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=5, decimal_places=2)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+
+
+#Área do cliente
+class Carrinho(models.Model):
+    pass
+
+class Pedido(models.Model):
+    pass
